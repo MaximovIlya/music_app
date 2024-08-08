c2e719e97d83cde895df82943200ad7df4276fbe
unknown <dilershina@gmail.com>
2024-08-08 18:14:46 +0300

    initial commit

---
+++ lib/core/configs/assets/app_images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+
+  static const String basePath = 'assets/images/';
+
+  static const String introbg = '${basePath}intro_bg.png';
+
+  static const String choooseModeBG = '${basePath}choose_mode_bg.png';
+
+  static const String authBG = '${basePath}auth_bg.png';
+}
